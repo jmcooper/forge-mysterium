3b481a21432a7618941c823f13f57e2c2e4b3e2b
Jim Cooper <jim@cooperplanet.com>
2020-10-20 22:49:11 -0600

    Set Mysterium spawn rate to rare

--- src/main/java/com/cooperplanet/mysteriummod/world/ModWorldGenerator.java
+++ src/main/java/com/cooperplanet/mysteriummod/world/ModWorldGenerator.java
@@ -22,7 +22,7 @@ public class ModWorldGenerator implements IWorldGenerator {
 	}
 	
 	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		generateOre(ModBlocks.MYSTERIUM_BLOCK.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, random.nextInt(7) + 4, 18);
+		generateOre(ModBlocks.MYSTERIUM_BLOCK.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, random.nextInt(7) + 4, 2);
 	}
 	
 	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances) {
